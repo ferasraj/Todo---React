d35d2c46b96792b931d71a452057b0cf995bc44d
ferasraj <feras.raj@gmail.com>
2025-03-25 06:10:54 +0300

    finish todo list :)

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,7 +1,85 @@
+import React, { useState, useRef, useEffect } from "react";
 import "./CSS/Todo.css";
+import Todoitems from "./Todoitems";
+
+let count = 0;
 
 const Todo = () => {
-  return <div className="todo"></div>;
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const add = () => {
+    if (inputRef.current.value.trim() === "") {
+      // تجاهل الإدخال إذا كان فارغًا
+      return;
+    }
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]),
+      (inputRef.current.value = "");
+    localStorage.setItem("todos_count", count);
+  };
+  // التركيز التلقائي على حقل الإدخال عند تحميل الصفحة
+  useEffect(() => {
+    inputRef.current.focus(); // التركيز على الحقل
+  }, []);
+
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+    count = localStorage.getItem("todos_count");
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, 100);
+  }, [todos]);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      add(); // استدعاء الدالة add عند الضغط على Enter
+    }
+  };
+
+  return (
+    <div className="todo">
+      <div className="todo-header">To Do List</div>
+      <div className="todo-add">
+        <input
+          onKeyPress={handleKeyPress}
+          ref={inputRef}
+          type="text"
+          placeholder="Add Your Task"
+          onInput={(e) => {
+            e.target.placeholder = ""; // إزالة النص الوهمي عند التركيز
+          }}
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
+      </div>
+      <div className="todo-list">
+        {todos.map((item, index) => {
+          return (
+            <Todoitems
+              key={index}
+              setTodos={setTodos}
+              no={item.no}
+              display={item.display}
+              text={item.text}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,6 +1,57 @@
 import "./CSS/Todoitems.css";
-const Todoitems = () => {
-  return <div>Todoitems</div>;
+import Closed from "./Assets/Closed.png";
+import Cancelled from "./Assets/Cancelled.png";
+import Complete from "./Assets/Complete.png";
+
+const Todoitems = ({ no, text, display, setTodos }) => {
+  const del = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo) => todo.no !== no);
+    setTodos(data);
+  };
+
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
+          data[i].display = "line-through";
+        } else {
+          data[i].display = "";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+    localStorage.setItem("todos", JSON.stringify(data));
+  };
+
+  return (
+    <div className="todoitems">
+      <div
+        className={`todoitems-container ${display}`}
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img src={Closed} alt="" />
+        ) : (
+          <img className="complete-img" src={Complete} alt="" />
+        )}
+
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img
+        onClick={() => {
+          del(no);
+        }}
+        className="cancel-img"
+        src={Cancelled}
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Todoitems;
